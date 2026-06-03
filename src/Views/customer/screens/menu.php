2b095bc354a8dd33787345bbadaca7a700628e20
Rbronze4 <a7473076@gmail.com>
2026-06-03 16:45:46 +0900

    注文画面のスタイルを改善し、メニューセクションに新しい要素を追加

--- src/Views/customer/screens/menu.php
+++ src/Views/customer/screens/menu.php
@@ -1,10 +1,19 @@
 <section id="menuScreen" class="screen menu-screen">
     <div id="categoryTabs" class="category-tabs"></div>
 
-    <div id="menuGrid" class="menu-grid"></div>
+    <main class="menu-panel">
+        <div id="menuGrid" class="menu-grid"></div>
+    </main>
 
     <nav class="bottom-nav">
-        <button id="historyButton" type="button">注文履歴</button>
-        <button id="cartButton" type="button">カート</button>
+        <button id="historyButton" type="button" class="history-nav-button">
+            <span class="history-icon">□</span>
+            <span>注文履歴</span>
+        </button>
+
+        <button id="cartButton" type="button" class="cart-nav-button">
+            <span>カートを見る</span>
+            <span class="cart-icon">🛒</span>
+        </button>
     </nav>
 </section>
