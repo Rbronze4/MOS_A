cba0e17528d20c6f5012035a1bd0301bcbfb0c20
Rbronze4 <a7473076@gmail.com>
2026-06-08 03:15:43 +0900

    ハンバーガーバーを別ファイルで管理する

---
+++ src/Views/staff/parts/side_menu.php
@@ -0,0 +1,13 @@
+<div id="sideMenuLayer" class="side-menu-layer">
+    <div class="side-menu">
+        <button id="closeMenuButton" class="menu-close-button" type="button">×</button>
+
+        <button type="button" onclick="location.href='/MOS_A/public/staff'">ホーム</button>
+        <button type="button" onclick="location.href='/MOS_A/public/staff/order-entry'">スタッフ注文</button>
+        <button type="button" onclick="location.href='/MOS_A/public/staff'">注文一覧</button>
+        <button type="button" onclick="location.href='/MOS_A/public/staff'">顧客詳細</button>
+        <button type="button" onclick="location.href='/MOS_A/public/staff'">商品管理</button>
+        <button type="button" onclick="location.href='/MOS_A/public/staff'">QR発行</button>
+        <button type="button" onclick="location.href='/MOS_A/public/staff'">ログアウト</button>
+    </div>
+</div>
