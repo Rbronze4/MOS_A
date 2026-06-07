e106640ccbe87b8c2b315d1b1dc31b61f5e4e014
Rbronze4 <a7473076@gmail.com>
2026-06-08 03:14:01 +0900

    店舗選択をプルダウンにする

--- src/Views/staff/screens/login.php
+++ src/Views/staff/screens/login.php
@@ -4,7 +4,19 @@
     <div class="login-form">
         <label>
             <span>店舗選択</span>
-            <input id="loginId" type="text">
+            <select id="storeId" name="storeId" required>
+                <option value="" disabled selected hidden>店舗を選択してください</option>
+                <option value="MH">緑橋本店</option>
+                <option value="MN">森ノ宮店</option>
+                <option value="TZ">玉造店</option>
+                <option value="TH">鶴橋店</option>
+                <option value="IM">今里店</option>
+                <option value="FB">深江橋店</option>
+                <option value="TY">谷町四丁目店</option>
+                <option value="HM">本町店</option>
+                <option value="KB">京橋店</option>
+                <option value="NB">なんば店</option>
+            </select>
         </label>
 
         <label>
@@ -12,7 +24,7 @@
             <input id="loginPassword" type="password">
         </label>
 
-        <button id="loginButton" class="white-button">ログイン</button>
+        <button id="loginButton" class="white-button" type="button">ログイン</button>
         <p id="loginError" class="error-text"></p>
     </div>
 </section>
